de7d157f634e94303ee9758385c405b44c55cb79
James <jameskirkmaynard@gmail.com>
2015-02-20 10:19:52 -0800

    Write error message with passing specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var Pizza = {
   sizePizza: 0,
+  typeOfPizza: [],
   slices: function(sizePizza) {
-    var oneSlice = 14.1375
-    var area = 3.14 * ((sizePizza/2)*(sizePizza/2))
-    return Math.round(area/oneSlice)
-  }
+    if (sizePizza <= 11 || sizePizza >= 25) {
+      return "Whoops!"
+    } else
+        var oneSlice = 14.1375
+        var area = 3.14 * ((sizePizza/2)*(sizePizza/2))
+        return Math.round(area/oneSlice)
+    }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,12 +2,19 @@ describe("Pizza", function() {
   describe("slices", function() {
     it("returns how many slices of pizza will be cut based on size of pizza in inches", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.sizePizza = 12
+      testPizza.sizePizza = 12;
       expect(testPizza.slices(12)).to.equal(8);
 
       var testPizza2 = Object.create(Pizza);
-      testPizza2.sizePizza = 24
-      expect(testPizza2.slices(24)).to.equal(32)
+      testPizza2.sizePizza = 24;
+      expect(testPizza2.slices(24)).to.equal(32);
+
+    });
+
+    it("will error if inches are less than 12 or greater than 24", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.sizePizza = 11;
+      expect(testPizza.slices(11)).to.equal("Whoops!")
     });
   });
 });
