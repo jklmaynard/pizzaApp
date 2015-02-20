3afbed22b97dc30e683cb51a128b788677ccc406
James <jameskirkmaynard@gmail.com>
2015-02-20 10:23:39 -0800

    Spruce up the spec code just a little bit, to make sure for full functionality.

--- spec/specs.js
+++ spec/specs.js
@@ -3,18 +3,21 @@ describe("Pizza", function() {
     it("returns how many slices of pizza will be cut based on size of pizza in inches", function() {
       var testPizza = Object.create(Pizza);
       testPizza.sizePizza = 12;
-      expect(testPizza.slices(12)).to.equal(8);
+      expect(testPizza.slices(testPizza.sizePizza)).to.equal(8);
 
       var testPizza2 = Object.create(Pizza);
       testPizza2.sizePizza = 24;
-      expect(testPizza2.slices(24)).to.equal(32);
-
+      expect(testPizza2.slices(testPizza2.sizePizza)).to.equal(32);
     });
 
     it("will error if inches are less than 12 or greater than 24", function() {
       var testPizza = Object.create(Pizza);
       testPizza.sizePizza = 11;
-      expect(testPizza.slices(11)).to.equal("Whoops!")
+      expect(testPizza.slices(testPizza.sizePizza)).to.equal("Whoops!")
+
+      var testPizza2 = Object.create(Pizza);
+      testPizza2.sizePizza = 25;
+      expect(testPizza2.slices(testPizza2.sizePizza)).to.equal("Whoops!");
     });
   });
 });
