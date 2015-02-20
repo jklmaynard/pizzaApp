7f733f3f262f1e154e8e922a13c64447da2dd7be
James <jameskirkmaynard@gmail.com>
2015-02-20 11:26:03 -0800

    Add jQuery functionality to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 var Pizza = {
   sizePizza: 0,
-  typeOfPizza: [],
   slices: function(sizePizza) {
     if (sizePizza <= 11 || sizePizza >= 25) {
-      return "Whoops!"
+      return "Whoops! You have an order too large or too small."
     } else
         var oneSlice = 14.1375
         var area = 3.14 * ((sizePizza/2)*(sizePizza/2))
         return Math.round(area/oneSlice)
     }
 }
+
+$(document).ready(function(event) {
+
+  $("form#orderPizza").submit(function(event) {
+    event.preventDefault();
+
+    var newPizza = Object.create(Pizza);
+    var inputtedName = $("input#name").val();
+    var inputtedPizzaSize = parseInt($("input#pizzaSize").val());
+    var inputtedPizzaType = $("select#pizzaType").val();
+    var slices = newPizza.slices(inputtedPizzaSize)
+
+    newPizza.orderName = inputtedName;
+    newPizza.sizePizza = inputtedPizzaSize;
+    newPizza.typeOfPizza = inputtedPizzaType;
+
+  $("div#newOrder").append("<p>" +
+      "Name: " + newPizza.orderName + "<br>" +
+      "Type: " + newPizza.typeOfPizza + "<br>" +
+      "Size: " + newPizza.sizePizza + " inches" + "<br>" +
+      "How many slices? " + slices + "</p>")
+
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -13,11 +13,11 @@ describe("Pizza", function() {
     it("will error if inches are less than 12 or greater than 24", function() {
       var testPizza = Object.create(Pizza);
       testPizza.sizePizza = 11;
-      expect(testPizza.slices(testPizza.sizePizza)).to.equal("Whoops!")
+      expect(testPizza.slices(testPizza.sizePizza)).to.equal("Whoops! You have an order too large or too small.")
 
       var testPizza2 = Object.create(Pizza);
       testPizza2.sizePizza = 25;
-      expect(testPizza2.slices(testPizza2.sizePizza)).to.equal("Whoops!");
+      expect(testPizza2.slices(testPizza2.sizePizza)).to.equal("Whoops! You have an order too large or too small.");
     });
   });
 });
